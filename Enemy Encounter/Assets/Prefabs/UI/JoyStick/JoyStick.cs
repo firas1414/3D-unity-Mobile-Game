527139e6ebb79f600824f1674539596ca45c45e5
MazenDamregi <damergimazen1234@gmail.com>
2024-03-03 17:39:27 +0100

    Move the Joystick with Touch Input

--- Enemy Encounter/Assets/Prefabs/UI/JoyStick/JoyStick.cs
+++ Enemy Encounter/Assets/Prefabs/UI/JoyStick/JoyStick.cs
@@ -5,19 +5,30 @@ using UnityEngine.EventSystems;
 
 public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
-    public void OnDrag(PointerEventData eventData)
-    {
-        Debug.Log($"On Drag Fired (eventData.position)");
-    }
+    [SerializeField] RectTransform ThumbStickTrans;
+    [SerializeField] RectTransform BackgroundTrans;
+    [SerializeField] RectTransform CenterTrans;
+
+public void OnDrag(PointerEventData eventData)
+{
+    Debug.Log($"On Drag Fired {eventData.position}");
+    Vector2 TouchPos = eventData.position;
+    Vector2 centerPos = BackgroundTrans.position;
+    Vector2 localOffset = Vector2.ClampMagnitude(TouchPos - centerPos,50);
+    ThumbStickTrans.position = centerPos + localOffset;
+}
+
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Debug.Log("Pointer Down");
+        BackgroundTrans.position = eventData.position;
+        ThumbStickTrans.position = eventData.position;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Debug.Log("Pointer Up");
+        BackgroundTrans.position = CenterTrans.position;
+        ThumbStickTrans.position = BackgroundTrans.position;
     }
 
     void Start()
