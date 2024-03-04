90f0941a31c795998896d9cdb1c20d49e613e64e
MazenDamregi <damergimazen1234@gmail.com>
2024-03-04 01:14:42 +0100

    Move the Player using Joystick Input

--- Enemy Encounter/Assets/Prefabs/UI/JoyStick/JoyStick.cs
+++ Enemy Encounter/Assets/Prefabs/UI/JoyStick/JoyStick.cs
@@ -5,6 +5,8 @@ using UnityEngine.EventSystems;
 
 public class JoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
 {
+    public delegate void OnStickInputValueUpdated(Vector2  inputVal);
+    public event OnStickInputValueUpdated OnStickValueUpdated;
     [SerializeField] RectTransform ThumbStickTrans;
     [SerializeField] RectTransform BackgroundTrans;
     [SerializeField] RectTransform CenterTrans;
@@ -14,8 +16,11 @@ public void OnDrag(PointerEventData eventData)
     Debug.Log($"On Drag Fired {eventData.position}");
     Vector2 TouchPos = eventData.position;
     Vector2 centerPos = BackgroundTrans.position;
-    Vector2 localOffset = Vector2.ClampMagnitude(TouchPos - centerPos,50);
+    Vector2 localOffset = Vector2.ClampMagnitude(TouchPos - centerPos,BackgroundTrans.sizeDelta.x/2);
+    Vector2 inputVal = localOffset / BackgroundTrans.sizeDelta.x/4;
     ThumbStickTrans.position = centerPos + localOffset;
+    OnStickValueUpdated?.Invoke(inputVal);
+
 }
 
 
@@ -29,6 +34,7 @@ public void OnDrag(PointerEventData eventData)
     {
         BackgroundTrans.position = CenterTrans.position;
         ThumbStickTrans.position = BackgroundTrans.position;
+        OnStickValueUpdated?.Invoke(Vector2.zero);
     }
 
     void Start()
