1b259deed75b1cfb9761028e0adb3822be363f6a
FIRAS AMARA <160745090+firas1414@users.noreply.github.com>
2024-03-08 03:50:22 +0100

    Add Aiming Control

--- Enemy Encounter/Assets/Prefabs/Player/Player.cs
+++ Enemy Encounter/Assets/Prefabs/Player/Player.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] JoyStick joystick_1; //Created an instance
+    [SerializeField] JoyStick moveStick; //Created an instance
+    [SerializeField] JoyStick aimStick;
     [SerializeField] CharacterController characterController;
-    [SerializeField] CameraController cameraController;
+    [SerializeField] CameraController cameraController; // aalech khalitha SerializeField ?
     [SerializeField] float moveSpeed = 20f;
     [SerializeField] float  turnSpeed = 30f;
     
     Vector2 DistanceMovedInput;
+    Vector2 aimInput;
     Camera mainCam;
 
 
     // Start is called before the first frame update
     void Start()
     {
-     joystick_1.OnStickValueUpdated += GetDistanceMoved; //subscribe the GetDistanceMoved function to the OnStickValueUpdated Event
+     moveStick.OnStickValueUpdated += GetDistanceMoved; //subscribe the GetDistanceMoved function to the OnStickValueUpdated Event
+     aimStick.OnStickValueUpdated +=aimStickUpdated;
      mainCam = Camera.main;
      /*
      When you do joystick_1.event_name += function_name, you are subscribing function_name to the event_name.
@@ -26,7 +29,17 @@ public class Player : MonoBehaviour
      */
     }
 
+    //turn the 2D direction to 3D direction
+    Vector3 StickInputToWorldDirection (Vector2 inputValue) {
+        Vector3 x_axis = mainCam.transform.right;
+        Vector3 z_axis = Vector3.Cross(x_axis, Vector3.up);
+        return x_axis * inputValue.x + z_axis * inputValue.y ; //convert the input to the world direction
+        
+    }
 
+    void aimStickUpdated(Vector2 inputValue){
+        aimInput = inputValue;
+     }
     void GetDistanceMoved(Vector2 inputValue)
     {
         DistanceMovedInput = inputValue;
@@ -36,22 +49,49 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 x_axis = mainCam.transform.right;
-        Vector3 z_axis = Vector3.Cross(x_axis, Vector3.up);
-        Vector3 MoveDir= x_axis * DistanceMovedInput.x + z_axis * DistanceMovedInput.y ; //where we are moving
-        characterController.Move(MoveDir* Time.deltaTime * moveSpeed); //Move the character
-        if (DistanceMovedInput.magnitude != 0 )
-        {
-            //we put it here because we don't want the direction to snap back when we stop moving with the joystock
-            // we want some animation when the player move from looking up to looking down instantly (rotaion progress => lerp to the rotaion instead of turn to it lerp=turn from one rotation to an other with alpha)
-            float turnLerpAlpha = turnSpeed* Time.deltaTime; //calculating alpha
-            transform.rotation=Quaternion.Lerp( transform.rotation,Quaternion.LookRotation(MoveDir,Vector3.up), turnSpeed);//player face the direction (vector3.up to direct the head of the player to the direction)
-            if(cameraController != null){
-            cameraController.AddYawInput(DistanceMovedInput.x);
-            }
+        PerformMoveAndAim();
+        UpdateCamera();
+    }
 
-             }
-           
+// make this function just to clear the code
+    private void PerformMoveAndAim(){
+        Vector3 MoveDir= StickInputToWorldDirection( DistanceMovedInput) ; //control the move direction
+        characterController.Move(MoveDir* Time.deltaTime * moveSpeed); //Move the character
+        UpdateAim(MoveDir);
+
+    }
+
+    private void  UpdateAim(Vector3 currentMoveDir){
+        Vector3 AimDir= MoveDir ; //control the aim direction
+        //ckech if the player is trying to aim (if we are not aiming aim is where we are moving)
+        if(aimInput.magnitude != 0){
+            AimDir= StickInputToWorldDirection(aimInput);
+
         }
+        RotationTowards(AimDir);
+    }
+
+    private void UpdateCamera(){
+        // don't update camera direction while aiming (if the player move and don't aim)
+        if (DistanceMovedInput.magnitude != 0 && aimInput.magnitude == 0 && cameraController != null) 
+        {
+            
+            cameraController.AddYawInput(DistanceMovedInput.x);
+            
+
+        }
+    }
+
+    private void RotationTowards(Vector3 AimDir){
+        if(AimDir.magnitude != 0){
+            
+            // we want some animation when the player move from looking up to looking down instantly (rotaion progress => lerp to the rotaion instead of turn to it lerp=turn from one rotation to an other with alpha)
+            // we want the player to aim in a direction while moving to another direction when he is attacked by the enemeies
+            //when you aim the direction of the player is controlled by the aim not the move (aim independent from the move)
+            float turnLerpAlpha = turnSpeed* Time.deltaTime; //calculating alpha
+            transform.rotation=Quaternion.Lerp( transform.rotation,Quaternion.LookRotation(AimDir,Vector3.up), turnLerpAlpha);//player aim the direction (vector3.up to direct the head of the player to the direction)
+        }
+    }
+    
         
 }
