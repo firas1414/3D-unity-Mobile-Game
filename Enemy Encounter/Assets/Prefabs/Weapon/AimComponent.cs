44fff8b1ec141d6c6405751862499c004de39ec4
MazenDamregi <damergimazen1234@gmail.com>
2024-03-14 21:08:28 +0100

    Implement the Aim Component

---
+++ Enemy Encounter/Assets/Prefabs/Weapon/AimComponent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimComponent : MonoBehaviour
+{
+    [SerializeField] Transform muzzle;
+    [SerializeField] float aimRange = 1000;
+    [SerializeField] LayerMask aimMask;
+
+
+
+    public GameObject GetAimTarget(){
+
+        Vector3 aimStart = muzzle.position;
+        if(Physics.Raycast(aimStart, GetAimDir(), out RaycastHit hitInfo, aimRange, aimMask))
+        {
+            return hitInfo.collider.gameObject;
+        }
+        return null;
+    }
+
+        private void OnDrawGizmos(){
+            Gizmos.DrawLine(muzzle.position, muzzle.position + GetAimDir() * aimRange);
+        }
+
+        Vector3 GetAimDir(){
+            Vector3 muzzleDir = muzzle.forward;
+            return new Vector3(muzzleDir.x, 0f, muzzleDir.z).normalized;
+        }
+
+}
