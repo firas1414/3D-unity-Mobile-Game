fba9b7b1831ed6f1786b38e98ba447f5d81721b5
MazenDamregi <damergimazen1234@gmail.com>
2024-03-29 17:42:39 +0100

    Create the Forgetting Routine

--- Enemy Encounter/Assets/Prefabs/Framework/AI/SenseComponent.cs
+++ Enemy Encounter/Assets/Prefabs/Framework/AI/SenseComponent.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public abstract class SenseComponent : MonoBehaviour
 {
+    [SerializeField] float forgettingTime = 3f;
+
     static List<PerceptionStimuli> registeredStimulis = new List<PerceptionStimuli>(); // Static means that the list is the same for the whole class, and every instance will have that same list
     List<PerceptionStimuli> PerceivableStimulis = new List<PerceptionStimuli>();
+    Dictionary<PerceptionStimuli, Coroutine> ForgettingRoutines = new Dictionary<PerceptionStimuli, Coroutine>();
+
+    public delegate void OnPerceptionUpdated(PerceptionStimuli stimuli, bool successfulySensed);
+    public event OnPerceptionUpdated onPerceptionUpdated;
 
     static public void RegisterStimuli(PerceptionStimuli stimuli)
     {
@@ -26,19 +32,39 @@ public abstract class SenseComponent : MonoBehaviour
     {
         foreach(var stimuli in registeredStimulis){
             if(IsStimuliSensable(stimuli)){
-                if(!PerceivableStimulis.Contains(stimuli)){ // The object just sensed
+                if(!PerceivableStimulis.Contains(stimuli)){ // The object just sensed the player
                     PerceivableStimulis.Add(stimuli);
-                    Debug.Log($"I sensed{stimuli.gameObject}");
+                    if(ForgettingRoutines.TryGetValue(stimuli, out Coroutine routine)) // If there is already a routine going on, stop it because the enemmy sensed
+                    {
+                        StopCoroutine(routine);
+                        ForgettingRoutines.Remove(stimuli);
+                        Debug.Log($"I sensed{stimuli.gameObject}");
+                    }
+                    else // If there is not a routine...
+                    {
+                        onPerceptionUpdated?.Invoke(stimuli, true);
+                        
+                    }
                 }
             }
             else{
                 if(PerceivableStimulis.Contains(stimuli)){
                     PerceivableStimulis.Remove(stimuli); // The object lost sense
+                    ForgettingRoutines.Add(stimuli, StartCoroutine(ForgetStimuli(stimuli))); // Started Forgetting routine
                     Debug.Log($"I lost sense{stimuli.gameObject}");
                 }
             }
         }
     }
+
+    IEnumerator ForgetStimuli(PerceptionStimuli stimuli)
+    {
+        yield return new WaitForSeconds(forgettingTime);
+        ForgettingRoutines.Remove(stimuli);
+        onPerceptionUpdated?.Invoke(stimuli, false);
+        Debug.Log($"I lost track of {stimuli.gameObject}");
+    }
+
     protected virtual void DrawDebug()
     {
 
--- Enemy Encounter/Assets/Prefabs/Framework/AI/SightSense.cs
+++ Enemy Encounter/Assets/Prefabs/Framework/AI/SightSense.cs
@@ -6,25 +6,29 @@ public class SightSense : SenseComponent
 {
     [SerializeField] private float sightDistance = 5f;
     [SerializeField] private float sightHalfAngle = 5f;
-    [SerializeField] private float eyeHeight = 1f;
+    [SerializeField] private float eyeHeight = 1f; // eyeHeight determines how high off the ground the raycast starts from.
 
 
 	protected override bool IsStimuliSensable(PerceptionStimuli stimuli)
     {
-        float distance = Vector3.Distance(stimuli.transform.position, transform.position);
+        float distance = Vector3.Distance(stimuli.transform.position, transform.position); // Distance between player and enemy
         if(distance > sightDistance)
         {
             return false;
         }
         Vector3 forwardDir = transform.forward; // Enemy forward direction
-        Vector3 stimuliDir = (stimuli.transform.position - transform.position).normalized; // Player's forward direction
-        if(Vector3.Angle(forwardDir, stimuliDir) > sightHalfAngle) // Check if player is the enemy's sight angle or not
+        Vector3 stimuliDir = (stimuli.transform.position - transform.position).normalized; // Directon from enemy to player
+        /*
+        Normalizing a vector means scaling it to have a length of 1 while preserving its direction.
+        This is important for directional calculations because it ensures that the length of the vector does not affect the comparison of directions.
+        */
+        if(Vector3.Angle(forwardDir, stimuliDir) > sightHalfAngle) // Check if player is in the enemy's sight angle or not
         {
             return false;
         }
-        if(Physics.Raycast(transform.position + Vector3.up * eyeHeight, stimuliDir, out RaycastHit hitInfo, sightDistance)) // Chck wether the player is standing behind an object
+        if(Physics.Raycast(transform.position + (Vector3.up * eyeHeight), stimuliDir, out RaycastHit hitInfo, sightDistance)) // Check wether the player is standing behind an object
         {
-            if(hitInfo.collider.gameObject != stimuli.gameObject)
+            if(hitInfo.collider.gameObject != stimuli.gameObject) // If the raycast is not hitting the player then it's defiently hitting another object, then we can't see the player
             {
                 return false;
             }
@@ -42,4 +46,5 @@ public class SightSense : SenseComponent
         Gizmos.DrawLine(drawCenter, drawCenter + leftLimitDir * sightDistance);
         Gizmos.DrawLine(drawCenter, drawCenter + rightLimitDir * sightDistance);
     }
+
 }
