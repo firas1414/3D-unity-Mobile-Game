b02570c3eda2e333fce01ed5de9c6c3a37865315
FIRAS AMARA <160745090+firas1414@users.noreply.github.com>
2024-04-09 17:53:25 +0100

    behavior trre (Compositors)

---
+++ Enemy Encounter/Assets/Prefabs/Framework/AI/BehaviorTree/Compositor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Compositor : BTNode
+{
+    //list of the tasks(nodes)
+    LinkedList<BTNode> children = new LinkedList<BTNode>();
+    //represent one task (item of the list)
+    LinkedListNode<BTNode> currentChild = null;
+
+    protected override NodeResult Execute(){
+        //No multiTask
+        if(children.Count == 0){
+            return NodeResult.Success;
+        }
+
+        currentChild = children.First;
+        return NodeResult.InProgress ;
+    }
+
+    //Switch between Tasks
+    protected bool Next(){
+        
+        if(currentChild != children.Last){
+            currentChild= currentChild.Next;
+            return true;
+        }
+        return false;
+    }
+
+    protected override void End(){
+        currentChild=null;
+    }
+}
