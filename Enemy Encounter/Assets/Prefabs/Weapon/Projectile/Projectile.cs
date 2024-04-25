f8a455fcdecaa049093923fea070cfd3d0ade21b
MazenDamregi <damergimazen1234@gmail.com>
2024-04-25 23:51:55 +0100

    Comments

--- Enemy Encounter/Assets/Prefabs/Weapon/Projectile/Projectile.cs
+++ Enemy Encounter/Assets/Prefabs/Weapon/Projectile/Projectile.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+// PROJECTILE IS THE PARTICLE ISSUED FROM THE SPITTER'S MOUTH WHEN ATTACKING THE PLAYER
 // USED FOR MAKING THE SPITTING MECHANISM, THIS CLASS SHOULD BE APPLIED ON THE PROJECTILE PREFAB
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float FlightHeight;
     [SerializeField] Rigidbody rigidBody;
-    [SerializeField] DamageComponent DamageComponent;
-    [SerializeField] ParticleSystem ExplosionVFX;
+    [SerializeField] DamageComponent DamageComponent; // YOU CAN APPLY THE "TriggreDamager" CLASS SINCE THAT CLASS IS INHERITED FROM THE "DamageComponent" CLASS
+    [SerializeField] ParticleSystem ExplosionVFX; // THIS PARTICLE IS RESPONSIBLE FOR MAKING THE EXPLOSION, IT IS SPAWNED ONCE THE FIRE HITS THE PLAYER'S COLLIDER
     ITeamInterface instigatorTeamInterface;
 
     public void Launch(GameObject instigator, Vector3 Destination)
@@ -20,6 +20,8 @@ public class Projectile : MonoBehaviour
         {
             DamageComponent.SetTeamInterfaceSrc(instigatorTeamInterface);
         }
+
+        // CALCULATING THE PARTICLE MOVEMENT MECHANISM
         float gravity = Physics.gravity.magnitude;
         float halfFlightTime = Mathf.Sqrt((FlightHeight * 2.0f) / gravity);
 
@@ -36,7 +38,7 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(instigatorTeamInterface.GetRelationTowards(other.gameObject) != ETeamRelation.Friendly)
+        if(instigatorTeamInterface.GetRelationTowards(other.gameObject) != ETeamRelation.Friendly) // CHECKS IF THE OBJECT THAT THE PARTICLE COLLIDED WITH IS A PLAYER OR NOT, IF YES DESTROY THE PARTICLE AND MAKE AN EXMPLOSION EFFECT
         {
             Explode();
         }
@@ -46,6 +48,6 @@ public class Projectile : MonoBehaviour
     {
         Vector3 pawnPos = transform.position;
         Instantiate(ExplosionVFX, pawnPos, Quaternion.identity);
-        Destroy(gameObject);
+        Destroy(gameObject); // DESTROY THE PARTICLE ONCE COLLIDED WITH THE PLAYER'S COLLIDER
     }
 }
