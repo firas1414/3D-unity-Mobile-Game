9183bd4ab19cafd02e6afbb37cae602144aaabcb
MazenDamregi <damergimazen1234@gmail.com>
2024-04-26 01:50:14 +0100

    Comments

--- Enemy Encounter/Assets/Prefabs/Framework/AI/BehaviorTree/CooldownDecorator.cs
+++ Enemy Encounter/Assets/Prefabs/Framework/AI/BehaviorTree/CooldownDecorator.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+// THIS CLASS CHECKS IF A CERTAIN DURATION IS FINISHED OR NOT BEFORE MAKING THE SPITTER'S ATTACK TASK AGAIN, SO THIS IS A CONDITION APPLIED ON THE SPITTER'S ATTACK TASK
 public class CooldownDecorator : Decorator
 {
     float cooldownTime;
@@ -15,36 +18,33 @@ public class CooldownDecorator : Decorator
 
     protected override NodeResult Execute()
     {
-        if (cooldownTime == 0)
-            return NodeResult.Inprogress;
+        if (cooldownTime == 0) // IF THERE IS NOT COOLDOWN TIME WHICH MEANS THERE IS NO CONDITION TO EXECUTE WHICH MEANS EXECUTE THE NEXT TASK
+            return NodeResult.Inprogress; // EXECUTE THE CHILD NODE
 
-        //first execution
-        if(lastExecutionTime == -1)
+        if(lastExecutionTime == -1) // IF THIS IS THE FIRST TIME THE ATTACK IS EXECUTED
         {
             lastExecutionTime = Time.timeSinceLevelLoad;
             return NodeResult.Inprogress;
         }
 
-        //cooldown not finished
-        if(Time.timeSinceLevelLoad - lastExecutionTime < cooldownTime)
+        if(Time.timeSinceLevelLoad - lastExecutionTime < cooldownTime) // IF COOLDOWN IS NOT FINISHED
         {
             if(failOnCooldown)
             {
                 return NodeResult.Failure;
             }
-            else
+            else // IF FALSE, IT WILL SUCCEED BUT NOT EXECUTE UNTIL THE COOLDOWN ENDS WHICH MEANS IT WON'T EXECUTE THE CHILD NODE BUT IT WILL KEEP CHECKING EVERYTIME IT COMES ACROSS THAT TASK
             {
                 return NodeResult.Success;
             }
         }
-
-        //cooldown is finished since last time
-        lastExecutionTime = Time.timeSinceLevelLoad;
-        return NodeResult.Inprogress;
+        // IF COOLDOWN IS FINISHED ----> PERMISSION TO MAKE THE ATTACK
+        lastExecutionTime = Time.timeSinceLevelLoad; 
+        return NodeResult.Inprogress; // EXECUTE THE CHILD NODE
     }
 
-    protected override NodeResult Update()
+    protected override NodeResult Update() // EXECUTED IF THE CONDITION IS MET(COOLDOWN TIME FINISHED)
     {
-        return GetChild().UpdateNode();
+        return GetChild().UpdateNode(); // EXECUTE THE CHILD NODE
     }
 }
